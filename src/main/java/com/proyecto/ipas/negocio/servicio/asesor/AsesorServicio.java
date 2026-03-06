f4f41c1a386ab9c172eb41220d513cf8d62ef3a5
01JOSE1 <oljd2002@gmail.com>
2026-03-05 22:39:36 -0500

    Negocio: crear servicio para calcular las estadisticas del dashboard del asesor

---
+++ src/main/java/com/proyecto/ipas/negocio/servicio/asesor/AsesorServicio.java
@@ -0,0 +1,48 @@
+package com.proyecto.ipas.negocio.servicio.asesor;
+
+import com.proyecto.ipas.datos.repositorio.ClienteRepositorio;
+import com.proyecto.ipas.datos.repositorio.PolizaRepositorio;
+import com.proyecto.ipas.datos.repositorio.UsuarioRepositorio;
+import com.proyecto.ipas.presentacion.excepcion.RecursoNOEncontradoException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Service
+public class AsesorServicio {
+
+    private static final Logger registro = LoggerFactory.getLogger(AsesorServicio.class);
+
+
+    PolizaRepositorio polizaRepositorio;
+    ClienteRepositorio clienteRepositorio;
+    UsuarioRepositorio usuarioRepositorio;
+
+    public AsesorServicio(PolizaRepositorio polizaRepositorio, ClienteRepositorio clienteRepositorio, UsuarioRepositorio usuarioRepositorio) {
+        this.polizaRepositorio = polizaRepositorio;
+        this.clienteRepositorio = clienteRepositorio;
+        this.usuarioRepositorio = usuarioRepositorio;
+    }
+
+    @Transactional(readOnly = true)
+    public Map<String, Long> obtenerDatosParaDashboard(Long idUsuario) {
+        usuarioRepositorio.findById(idUsuario).orElseThrow(() -> new RecursoNOEncontradoException("Usuario", "id", idUsuario));
+
+        Map<String, Long> datosDashboardAsesor = new LinkedHashMap<>();
+
+        datosDashboardAsesor.put("polizasCreadasMes", polizaRepositorio.contarPolizasEsteMes(idUsuario));
+        datosDashboardAsesor.put("gestionesMes", clienteRepositorio.contarGestionesMes(idUsuario));
+        datosDashboardAsesor.put("clientesGestionados", clienteRepositorio.contarClientesPorUsuario(idUsuario));
+
+        LocalDate limite = LocalDate.now().plusDays(8);
+        datosDashboardAsesor.put("polizasProximasVencer", polizaRepositorio.countPolizasPorVencer(limite));
+
+        return datosDashboardAsesor;
+
+    }
+}
